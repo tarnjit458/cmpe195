789769dec316331da965bb47551cd50e71c69c26
Grant Haack <granthaack@protonmail.com>
2019-03-04 14:17:37 -0800

    cleanup

--- convolutions/main.py
+++ convolutions/main.py
@@ -1,14 +1,12 @@
 from PIL import Image
-import numpy as np
 
 KSIZE = 3
 
 with Image.open("kitten.jpg") as raw_img:
     # Convert image to black and white with 8 bit color values
     img = raw_img.convert("L")
-    print(np.asarray(img))
     # Instantiate the kernel for the convolution
-    kernel = [[0, 1 , 0], [1, -4, 1], [0, 1, 0]]
+    kernel = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
     # Instantiate array for convolved image
     conv = []
     for p_y in range(0, img.height - KSIZE):
@@ -19,19 +17,10 @@ with Image.open("kitten.jpg") as raw_img:
                 for k_x in range(0, KSIZE):
                     conv_pix += img.getpixel((p_x + k_x, p_y + k_y)) * kernel[k_y][k_x]
             conv.append(conv_pix)
-    # conv = np.reshape(np.array(conv), (img.height - KSIZE, img.width - KSIZE))
     print(conv)
-    # conv_img = Image.fromarray(conv, "L")
     conv_img = Image.new("L", (img.width - KSIZE, img.height - KSIZE))
     conv_img.putdata(conv)
     print(conv_img.width)
     print(conv_img.height)
     conv_img.show()
     img.show()
-
-
-#for pix_x < img.width + kernel.width:
-    #for pix_y < img.height + kernel.heigh:
-        #iterate over the kernel, perform math on each corresponding point in the image
-        #use top left pixel (pix_x, pix_y) of kernel as a reference
-        #paste convoluted values in a new array
